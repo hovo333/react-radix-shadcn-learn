3c6c38abfa8dccaa49d31ff3568a96d7fb2a5b8d
Hovhannes Ghazaryan <hovhannes.ghazaryan@fcc.am>
2026-07-22 14:48:46 +0400

    Module 2 - Lesson 3:Add button component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
         <h1 className="text-4xl font-bold text-blue-600">
           React Course
         </h1>
+
+          <button className="bg-blue-600 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-red-600">Button</button>
       </main>
   );
 }
