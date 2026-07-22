3e049491dd4361b5197ec088fffdd4f4f8414798
Hovhannes Ghazaryan <hovhannes.ghazaryan@fcc.am>
2026-07-22 16:13:23 +0400

    Module 2 - Lesson 9: Add utility functions, button component, and update configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import {Button} from "@/components/ui/button.tsx";
+
 function App() {
   return (
       <main className="flex min-h-screen items-center justify-center">
         <h1 className="text-4xl font-bold text-blue-600">
           React Course
         </h1>
+          <br/>
 
           <button className="bg-blue-600 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-red-600">Button</button>
+
+          <Button variant="success">shadcn Button</Button>
+
+          <Button variant="outline" asChild>
+              <a href="/accounts">asChild Button</a>
+          </Button>
+
+          <Button variant="success">shadcn Button</Button>
       </main>
   );
 }
